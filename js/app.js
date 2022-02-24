2a82bcd0cea935e624b06be6a6d6c7a1add7ef9d
Frederick Roegiers <frederick@rogerthat.be>
2022-02-24 09:38:27 +0100

    init

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const app = {
+  init() {
+    this.cacheElements();
+    this.registerListeners();
+    this.createHTMLList();
+  },
+  cacheElements() {},
+  registerListeners() {},
+  createHTMLList() {},
+};
+
+app.init();
